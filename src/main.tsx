3c3c0aab96151a7cc8c3e0455357f81364f55f20
Sidhart Singh <isidhartsingh@gmail.com>
2023-07-26 17:22:17 +0530

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
